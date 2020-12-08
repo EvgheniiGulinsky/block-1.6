4d2ea6f7207b1c37d9330f06da47695234b9e419
Evgeni <riggedman22@gmail.com>
2020-12-08 14:49:10 +0300

    Fix console message

--- src/js/includes/links.js
+++ src/js/includes/links.js
@@ -4,6 +4,5 @@ links.forEach(link => link.addEventListener('click', linkActivate))
 
 function linkActivate(event){
     links.forEach(link => link.parentNode.classList.remove('header-nav__link--active'))
-    console.log(event.target)
     event.target.parentNode.classList.add('header-nav__link--active')
 }
